b57e34b5bbfadde3c1aded7c59de69b2c9a9dd53
Max Pfeiffer <max@maxpfeiffer.ch>
2025-09-19 18:28:59 +0200

    Configured Cilium load balancer and ArgoCD for HTTP access

--- kubernetes/helm_releases.tf
+++ kubernetes/helm_releases.tf
@@ -1,9 +1,48 @@
 resource "helm_release" "argocd" {
-  name       = "argo-cd"
-  namespace = "argocd"
+  name             = "argo-cd"
+  namespace        = "argocd"
   create_namespace = true
-  chart      = "argo-cd"
-  version    = "8.3.1"
-  repository = "https://argoproj.github.io/argo-helm"
-  timeout    = 120
+  chart            = "argo-cd"
+  version          = "8.3.1"
+  repository       = "https://argoproj.github.io/argo-helm"
+  timeout          = 120
+  set = [
+    {
+      name  = "global.domain"
+      value = var.argocd_domain
+    },
+    {
+      name  = "configs.params.server\\.insecure"
+      value = "true"
+    },
+    {
+      name  = "server.ingress.enabled"
+      value = "true"
+    },
+    {
+      name  = "server.ingress.ingressClassName"
+      value = "cilium"
+    },
+    {
+      name  = "server.ingress.annotations.ingress\\.cilium\\.io/force-https"
+      value = "disabled"
+    },
+  ]
+}
+
+resource "helm_release" "cilium_lb_config" {
+  depends_on = [helm_release.argocd]
+  name       = "cilium-lb-config"
+  chart      = "${path.module}/helm_charts/cilium-lb-config"
+  timeout    = 60
+  set = [
+    {
+      name  = "ciliumLoadBalancerIpRange.start"
+      value = var.cilium_load_balancer_ip_range_start
+    },
+    {
+      name  = "ciliumLoadBalancerIpRange.stop"
+      value = var.cilium_load_balancer_ip_range_stop
+    },
+  ]
 }
--- kubernetes/providers.tf
+++ kubernetes/providers.tf
@@ -1,10 +1,10 @@
 terraform {
-        required_providers {
+  required_providers {
     helm = {
       source  = "hashicorp/helm"
       version = "3.0.2"
     }
-        }
+  }
 }
 
 provider "helm" {
