121ab182937478f223dffecbc734323b30336155
Max Pfeiffer <max@maxpfeiffer.ch>
2026-08-23 10:54:39 +0200

    feat: made proxmox_download_file more resilient when installing multiple projects on the same Proxmox host

--- proxmox/iso_images.tf
+++ proxmox/iso_images.tf
@@ -1,7 +1,8 @@
 resource "proxmox_download_file" "talos_linux_iso_image" {
-  content_type = "iso"
-  datastore_id = "local"
-  node_name    = var.proxmox_target_node
-  url          = var.talos_linux_iso_image_url
-  file_name    = var.talos_linux_iso_image_filename
+  content_type        = "iso"
+  datastore_id        = "local"
+  node_name           = var.proxmox_target_node
+  url                 = var.talos_linux_iso_image_url
+  file_name           = var.talos_linux_iso_image_filename
+  overwrite_unmanaged = true
 }
